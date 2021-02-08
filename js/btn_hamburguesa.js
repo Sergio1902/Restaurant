91bdca91964685f6ac5f23ac5b65a706900a26e6
Sergio1902 <ser19car@gmail.com>
2021-02-08 11:31:30 -0500

    PANEL-FLOTANTE-HAMBURGUESA

---
+++ js/btn_hamburguesa.js
@@ -0,0 +1,29 @@
+export default function btnHamburguesa() {
+
+    const $PANEL = document.querySelector(".panel-flotante")
+
+    document.addEventListener("click",function (e) {
+         if(e.target.matches(".hamburguesa")){
+             $PANEL.classList.toggle("is-active-panel")
+        }       
+        if(e.target.matches(".panel-flotante-menu .menu-items a")){
+            $PANEL.classList.remove("is-active-panel")
+        }
+    })
+    // RESPONSIVE - PANEL FLOTANTE
+    let breakPoint = window.matchMedia("(min-width:1041px)")
+
+    // funcion expresada
+    const responsive = function (e) {
+        if(e.matches){
+            $PANEL.classList.remove("is-active-panel")
+            
+        }
+    }
+// add listener - utilizado solo en medias querys
+        breakPoint.addListener(responsive)
+
+
+
+
+}
--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
+import btnHamburguesa from "./btn_hamburguesa.js"
 import slider from "./slider.js"
 
 document.addEventListener("DOMContentLoaded", function (e) {
     slider(".next",".previ",".slider")
+    btnHamburguesa()
 })
